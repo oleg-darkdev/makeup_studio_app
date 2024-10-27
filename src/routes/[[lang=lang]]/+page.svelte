127277da9136d44d8c1c2d433b9179ab6f9ff0dd
DarkDev <darkdev_brand@riseup.net>
2024-10-28 01:04:33 +0200

    init basic structure

--- src/routes/[[lang=lang]]/+page.svelte
+++ src/routes/[[lang=lang]]/+page.svelte
@@ -1,35 +1,80 @@
 <script lang="ts">
 	// import { SEO } from '$sharedUtils';
-	// import { boardgamesList, categories } from '$sharedData';
-	// import { LineDivider, BigContaciDivider, SmallContaciDivider } from '$sharedUi';
-	// import {
-	// 	LinkinBio,
-	// 	Services,
-	// 	TagsLine,
-	// 	About,
-	// 	Gallery,
-	// 	Stats,
-	// 	Faq,
-	// 	FullWidthBanner,
-	// 	Features,
-	// 	Instructions,
-	// 	PleaseEnterPassword,
-	// 	BestBoardgames,
-	// 	Blog,
-	// 	ColourfulSection,
-	// 	ContactForm,
-	// 	BoardgamesList,
-	// 	Price,
-	// 	Team,
-	// 	FreeFirst,
-	// 	OurMission
-	// } from '$widgetsLanding';
+	import { boardgamesList, categories } from '$sharedData';
+	import { LineDivider, BigContaciDivider, SmallContaciDivider } from '$sharedUi';
+	import {
+		LinkinBio,
+		Services,
+		TagsLine,
+		About,
+		Gallery,
+		Stats,
+		Faq,
+		FullWidthBanner,
+		Features,
+		Instructions,
+		PleaseEnterPassword,
+		BestBoardgames,
+		Blog,
+		ColourfulSection,
+		ContactForm,
+		BoardgamesList,
+		Price,
+		Team,
+		FreeFirst,
+		OurMission
+	} from '$widgetsLanding';
 </script>
 
 <svelte:head>
 	<title>Puzzle Management | Home </title>
 	<meta name="description" content="" />
 </svelte:head>
+
+
+<!-- <SEO title="Home" /> -->
+
+<!-- <TagsLine /> -->
+
+<About {categories}/>
+
+<!-- <Stats /> -->
+
+<LineDivider />
+
+<OurMission />
+
+<LineDivider />
+
+<Features {categories}/>
+
+<SmallContaciDivider />
+
+<!-- <LineDivider /> -->
+
+<BoardgamesList {categories} />
+
+<LineDivider />
+
+
+
+<!-- <ColourfulSection /> -->
+
+
+<!-- <LineDivider /> -->
+
+<!-- <BestBoardgames /> -->
+
+<!-- <LineDivider /> -->
+
+<!-- <Price /> -->
+
+<!-- <Services /> -->
+
+<!-- <BigContaciDivider /> -->
+
+<Faq />
+
 
 
 <style lang="postcss">
