4a74913ed7425f6d270c45519a647998605eb621
DarkDev <darkdev_brand@riseup.net>
2024-10-28 01:44:50 +0200

    init menu for mobile

--- src/lib/widgets/landing/Header.svelte
+++ src/lib/widgets/landing/Header.svelte
@@ -6,12 +6,42 @@
 	interface NavigationProps {
 		navigation: NavigationLink[];
 	}
-	let { navigation }: NavigationProps = $props();
+	let { navigation, boardgamesList }: NavigationProps = $props();
+
+
+	let showMenu = $state(true);
 </script>
 
 <div role="banner" class="navbar w-nav">
 	<div class="container-full w-container">
 		<div class="navbar-grid">
+			{#if showMenu}
+			<div class="sm-menu flex h-screen flex-col" onclick={()=> showMenu = !showMenu}>
+				<div class="flex flex-col text-center my-auto">
+					{#each navigation as { title, link }}<a
+							href={link}
+							aria-current="page"
+							class="w-nav-link text-white"><h2>{title}</h2></a
+						>{/each}
+					<!-- <a href="/" class="button-link">more about us</a> -->
+				</div>
+				<!-- <div class="footer-item px-10">
+					<div class="footer-title-wrap">
+						<img src="/images/boardgames-icon.svg" loading="lazy" alt="" class="footer-shape-1" />
+						<h6 class="footer-title">Lista gier</h6>
+					</div>
+					<div class="w-layout-grid grid-footer-menu ">
+						{#each boardgamesList as { title, progress, linkLanding, icon }}
+							<a href={progress.landing ? linkLanding : '#'} target="_blank" class="footer-link">
+								<img src={icon} class="h-8" alt={title} />
+								{title}
+							</a>
+						{/each}
+					</div>
+				</div> -->
+			</div>
+			{/if}
+
 			<div class="nav-left">
 				<nav class="nav-menu w-nav-menu">
 					<!-- w--current -->
@@ -19,14 +49,21 @@
 						<a href={link} aria-current="page" class="nav-link w-nav-link">{title}</a>
 					{/each}
 					<!-- <div class="w-dropdown">
-						<div class="dropdown-toggle nav-link w-dropdown-toggle">
-							<div>Pages</div>
-							<div class="dropdown-icon w-icon-dropdown-toggle"></div>
-						</div>
-						<nav class="dropdown-list w-dropdown-list">
-							<a href="/" aria-current="page" class="dropdown-link w-dropdown-link">Home</a>
-						</nav>
-					</div> -->
+
+<div class="dropdown-toggle nav-link w-dropdown-toggle">
+
+<div>Pages</div>
+
+<div class="dropdown-icon w-icon-dropdown-toggle"></div>
+
+</div>
+
+<nav class="dropdown-list w-dropdown-list">
+
+<a href="/" aria-current="page" class="dropdown-link w-dropdown-link">Home</a>
+
+</nav>
+                    </div> -->
 				</nav>
 			</div>
 
@@ -46,12 +83,67 @@
 				/>
 			</a>
 
-			<div class="menu-button w-nav-button">
-				<div class="w-icon-nav-menu"></div>
+			<div class="menu-button w-nav-button w--open" onclick={()=> showMenu = !showMenu}>
+				<!-- <div class="w-icon-nav-menu"></div> -->
+				<h4>=</h4>
 			</div>
 		</div>
 	</div>
 </div>
 
 <style lang="postcss">
+	@media screen and (min-width: 991px) {
+		.sm-menu,
+		.w-nav-button {
+			display: none;
+		}
+	}
+	@media screen and (max-width: 991px) {
+		.w-nav-button {
+			float: right;
+			cursor: pointer;
+			-webkit-tap-highlight-color: #0000;
+			tap-highlight-color: #0000;
+			-webkit-user-select: none;
+			user-select: none;
+			padding: 18px;
+			font-size: 24px;
+			/* display: none; */
+			position: relative;
+			color: #fff;
+			background-color: #c8c8c8;
+		}
+
+		.w-nav-button:focus {
+			outline: 0;
+		}
+
+		.menu-button {
+			border-radius: var(--border-radius--border-radius);
+			background-color: var(--color--primary-1);
+			color: var(--color--black);
+			margin-left: auto;
+		}
+
+		.menu-button.w--open {
+			background-color: var(--color--primary-2);
+			color: var(--color--black);
+		}
+
+		.sm-menu {
+			background: var(--color--gray-2);
+			min-width: 200px;
+			position: absolute;
+			top: 100%;
+			left: 0;
+			right: 0;
+			overflow: visible;
+		}
+	}
+
+	@media screen and (max-width: 479px) {
+		.menu-button {
+			padding: 12px;
+		}
+	}
 </style>
