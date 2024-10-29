11e26a571648bafa212cd486a33250b50b6c9184
DarkDev <darkdev_brand@riseup.net>
2024-10-29 22:43:31 +0200

    upd structure

--- src/routes/[[lang=lang]]/+page.svelte
+++ src/routes/[[lang=lang]]/+page.svelte
@@ -9,6 +9,29 @@
 		BoardgamesList,
 		OurMission
 	} from '$widgetsLanding';
+	// import { SEO } from '$sharedUtils';
+	// import {
+	// 	LinkinBio,
+	// 	Services,
+	// 	TagsLine,
+	// 	About,
+	// 	Gallery,
+	// 	Stats,
+	// 	Faq,
+	// 	FullWidthBanner,
+	// 	Features,
+	// 	Instructions,
+	// 	PleaseEnterPassword,
+	// 	BestBoardgames,
+	// 	Blog,
+	// 	ColourfulSection,
+	// 	ContactForm,
+	// 	BoardgamesList,
+	// 	Price,
+	// 	Team,
+	// 	FreeFirst,
+	// 	OurMission
+	// } from '$widgetsLanding';
 </script>
 
 <svelte:head>
@@ -16,9 +39,13 @@
 	<meta name="description" content="" />
 </svelte:head>
 
+<!-- <SEO title="Home" /> -->
+
 <TagsLine />
 
 <About {categories}/>
+
+<!-- <Stats /> -->
 
 <LineDivider />
 
@@ -30,11 +57,34 @@
 
 <SmallContaciDivider />
 
+<!-- <LineDivider /> -->
+
 <BoardgamesList {categories} />
 
+<!-- <ColourfulSection /> -->
+<!-- <BestBoardgames /> -->
+<!-- <Gallery /> -->
+<!-- <BestBoardgames /> -->
+<!-- <Price /> -->
+<!-- <BigContaciDivider /> -->
+<!-- <Services /> -->
 <LineDivider />
 
 <Faq />
+
+<!-- <Team /> -->
+
+<!-- <FreeFirst /> -->
+
+<!-- <FullWidthBanner /> -->
+
+<!-- <ContactForm /> -->
+
+<!-- <Instructions /> -->
+
+<!-- <PleaseEnterPassword /> -->
+
+<!-- <LinkinBio /> -->
 
 
 <style lang="postcss">
